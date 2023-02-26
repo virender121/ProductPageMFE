167a7e5c32a5cbe652b53c95cd7f9baace27f4d3
Kuldeep Dhaka <kuldeep.dhaka@techinfinito.com>
2023-02-27 02:49:01 +0530

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const dotenv = require("dotenv");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const Dotenv = require('dotenv-webpack');
 
 dotenv.config()
 const deps = require("./package.json").dependencies;
-module.exports = {
+module.exports = (env) => ({
   output: {
     publicPath: "http://localhost:3004/",
   },
@@ -42,33 +43,34 @@ module.exports = {
     ],
   },
 
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "ProductPageMFE",
-      filename: "remoteEntry.js",
-      remotes: {
-        HomePageMFE: `HomePageMFE@${process.env.HomePageMFE}remoteEntry.js`,
-        LoginPageMFE: `LoginPageMFE@${process.env.LoginPageMFE}remoteEntry.js`,
-        CheckoutPageMFE: `CheckoutPageMFE@${process.env.CheckoutPageMFE}remoteEntry.js`,
+  plugins: [new Dotenv(),
+
+  new ModuleFederationPlugin({
+    name: "ProductPageMFE",
+    filename: "remoteEntry.js",
+    remotes: {
+      HomePageMFE: `HomePageMFE@${process.env.HomePageMFE}remoteEntry.js`,
+      LoginPageMFE: `LoginPageMFE@${process.env.LoginPageMFE}remoteEntry.js`,
+      CheckoutPageMFE: `CheckoutPageMFE@${process.env.CheckoutPageMFE}remoteEntry.js`,
+    },
+    exposes: {
+      './SingleProductCard': './src/components/views/SingleProductCard/SingleProductCard.jsx',
+      './RatingStars': './src/components/views/RatingStars/RatingStars.jsx',
+    },
+    shared: {
+      ...deps,
+      react: {
+        singleton: true,
+        requiredVersion: deps.react,
       },
-      exposes: {
-        './SingleProductCard': './src/components/views/SingleProductCard/SingleProductCard.jsx',
-        './RatingStars': './src/components/views/RatingStars/RatingStars.jsx',
+      "react-dom": {
+        singleton: true,
+        requiredVersion: deps["react-dom"],
       },
-      shared: {
-        ...deps,
-        react: {
-          singleton: true,
-          requiredVersion: deps.react,
-        },
-        "react-dom": {
-          singleton: true,
-          requiredVersion: deps["react-dom"],
-        },
-      },
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-    }),
+    },
+  }),
+  new HtmlWebPackPlugin({
+    template: "./src/index.html",
+  }),
   ],
-};
+});
